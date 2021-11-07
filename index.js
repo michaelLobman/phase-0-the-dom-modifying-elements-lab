f5678e1d1d57afd67fa8862e7720121240f4115c
Michael Lobman <michael.lobman@gmail.com>
2021-11-07 17:12:54 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
+const main = document.getElementById("main");
 main.remove();
-const newHeader = document.createElement('h1');
-newHeader.id = "victory";
-newHeader.textContent ="Michael Lobman is the champion";
-document.body.append(newHeader);
-
-
-
-
-/*
-const createdHeader = document.createElement('h1');
-document.body.append(createdHeader);
-createdHeader.id = 'victory';
-const newHeader = document.querySelector('h1#victory');
-newHeader.textContent = "Michael Lobman is the champion";
 
 const newHeader = document.createElement('h1');
-document.body.append(newHeader);
 newHeader.id = 'victory';
 newHeader.textContent = "Michael Lobman is the champion";
-const newHeader = document.createElement('h1#victory');
-const newHeader = document.querySelector('h1#victory');
-const newHeader = document.getElementById('victory');
-const newHeader = document.querySelector('h1#victory');
-
-const newHeader = document.createElement('h1#victory');
 document.body.append(newHeader);
-newHeader.textContent="Michael Lobman is the champion.";
-*/
+
+
+
